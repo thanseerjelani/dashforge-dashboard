4807b49de5c13f952cf948268dbb4be727bf0c6a
thanseerjelani <thanseerjelani@gmail.com>
2025-09-10 20:40:33 +0530

    new commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { analyzer } from 'vite-bundle-analyzer'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(), // Add the Tailwind CSS plugin
+    analyzer(),
   ],
   resolve: {
     alias: {
